9275ac6d44f066115960d1993f4966a34c27cff3
qmm1997 <1342649572@qq.com>
2024-06-14 13:58:01 +0800

    feat：增加pina状态管理

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import 'virtual:windi.css'
 import './assets/style/reset.scss'
+import store from '@/stores'
 
 import App from '@/App.vue'
 import router from '@/router'
@@ -9,6 +10,7 @@ import router from '@/router'
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(store)
 app.use(router)
 
 app.mount('#app')
---
+++ src/stores/other.ts
@@ -0,0 +1,13 @@
+import { defineStore } from 'pinia'
+
+export const useOtherStore = defineStore('other', {
+  state: () => ({
+    var: '',
+  }),
+  actions: {
+    //
+  },
+  getters: {
+    //
+  },
+})
